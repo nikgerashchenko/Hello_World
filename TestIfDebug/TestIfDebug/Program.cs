e0c74d87661dc8e3761a00621b3aa22d066d5ce6
ARSIS\ngerashchenko <nikita.gerashchenko@arsis.ru>
2018-06-27 17:39:56 +0300

    + test proj

---
+++ TestIfDebug/TestIfDebug/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIfDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            #if DEBUG
+            Console.WriteLine("DEBUG is defined");
+            #endif
+
+            Console.Read();
+
+
+        }
+    }
+}
